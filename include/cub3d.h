fbf914f69911912be8031155b2daa24564ee2aaf
Marcelo Dias Machado <madias-m@c1r9p4.42sp.org.br>
2025-02-05 16:45:08 -0300

    ueh

--- include/cub3d.h
+++ include/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: madias-m <madias-m@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/05 14:14:49 by madias-m          #+#    #+#             */
-/*   Updated: 2025/02/05 14:35:10 by madias-m         ###   ########.fr       */
+/*   Updated: 2025/02/05 15:55:57 by madias-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,12 @@
 # define CUB3D_H
 
 # include <stdio.h>
+# include <stdlib.h>
+# include <stdbool.h>
 # include "../libft/libft.h"
+# include "../MLX42/include/MLX42/MLX42.h"
+
+#define WIDTH 512
+#define HEIGHT 512
 
 #endif
