7f773f91d7a4351d8884d8e7f262b4cc0abc8fe4
Marcelo Dias Machado <madias-m@c1r4p1.42sp.org.br>
2025-04-17 12:23:10 -0300

    isolando a iniciacao da janela e imagem para depois que a validacao eh feita

--- source/main.c
+++ source/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: babischa <babischa@student.42.fr>          +#+  +:+       +#+        */
+/*   By: madias-m <madias-m@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/05 14:07:08 by madias-m          #+#    #+#             */
-/*   Updated: 2025/04/14 17:28:17 by babischa         ###   ########.fr       */
+/*   Updated: 2025/04/17 12:20:45 by madias-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,15 @@ t_game	*game(void)
 }
 
 void	init_data(void)
+{
+	game()->ceiling = -42;
+	game()->floor = -42;
+	(game())->textures = ft_calloc(1, sizeof(t_textures));
+	(game())->coord = ft_calloc(1, sizeof(t_coordinates));
+	game()->map_list = NULL;
+}
+
+void	init_game_window(void)
 {
 	(game())->mlx = mlx_init(INIT_W, INIT_H, "CUB3D", true);
 	if (!(game()->mlx))
@@ -41,17 +50,13 @@ void	init_data(void)
 		puts(mlx_strerror(mlx_errno));
 		return ;
 	}
-	game()->ceiling = -42;
-	game()->floor = -42;
-	(game())->textures = ft_calloc(1, sizeof(t_textures));
-	(game())->coord = ft_calloc(1, sizeof(t_coordinates));
-	game()->map_list = NULL;
 }
 
 int	main(int argc, char **argv)
 {
 	init_data();
 	validate(argc, argv);
+	init_game_window();
 	set_player_initial_pos();
 	mlx_loop_hook(game()->mlx, render_loop, NULL);
 	mlx_loop_hook(game()->mlx, new_movement, game());
